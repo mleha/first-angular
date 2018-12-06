cf563231670a7100e9eaf972d7a5f23fc6e376ee
Mleha <mleha@mlehamleha.fvds.ru>
2018-12-06 19:12:49 +0300

    2018-12-07

--- js/angstart.js
+++ js/angstart.js
@@ -1,9 +1,5 @@
 (function() {
- 
-  'use strict';
- 
- 
- 
+ 'use strict';
  var myApp=
   angular
   .module('app', ['ngStorage'])
@@ -14,22 +10,26 @@
 								'all':{'name':'Все'},
 								'del':{'name':'Удаленные'}};
 			
-			// for(var sect in $scope.sections){
-				// $scope.sections[sect].css = {};
-				// $scope.sections[sect].css.data="btn-info";
-			// }
-			
 			pushCSS_ToObject($scope.sections, "btn-info");
 			
 			//console.log($scope.sections);
 			$scope.search = {};
+			$scope.search.id="";
+			//$scope.found = [];
+			$scope.imgdiv = 'img-hidden';
+			$scope.img_div_style = '';
 			
 			$scope.page = window.location.hash.substr(1) || 'all';
+			//console.log($scope.page);
+			if ($scope.page!="fav" && $scope.page != "del") {
+				$scope.page="all";
+				window.location.hash="#all";
+			}
 			$scope.sections[$scope.page].css.data="btn-warning";
 			$scope.section = $scope.sections[$scope.page];
 			//$scope.sectVar = $scope.page;//$localStorage.sectVar || 'all';
 			$scope.localEmojis =  $localStorage.emojis || [];
-			
+			$scope.found = $scope.localEmojis;
 			//$scope.stored++;
 
 			$http({
@@ -40,50 +40,144 @@
 				//console.log($scope.emojis);
 				updateEmojis( response.data);
 				//console.log($scope);
+				$scope.do_search();
 			});		
 		}
 		
+		//=======================================================
+		//    service functions
+		//------------------------------------------------		
 		function updateEmojis(emojis){
 			for(var id in emojis){
 				var found = $filter('getById')($scope.localEmojis, id);
 				
-				if (found != undefined){
-					//console.log(found);
+				if (found != undefined)
 					found.url=emojis[id];
-				}else{
+				else
 					$scope.localEmojis.push({'id':id,'url':emojis[id], 'cat':'all'});
-				}
 			}
 			$localStorage.emojis = $scope.localEmojis;
 			pushCSS_ToObject($scope.localEmojis, "img-transparent");
 		}
 		
-		function pushCSS_ToObject(arr,data,q=null){
+		function pushCSS_ToObject(arr,data){
 			angular.forEach(arr,function(item){
 				if (item.css == undefined) item.css = {'data':data} 
-				
 			});
 		}
-		
-		$scope.makeFavorive = function(emoji){
-			//console.log(emoji);
-			emoji.cat="fav";
-			emoji.css.data="img-no-transparent";
-		}
-		
+		//=======================================================
+		//    events/listeners
+		//------------------------------------------------
 		$scope.check_visible = function(button){
-			//console.log(button);
-			
 			if (button=="fav" && $scope.page=="all") return "img-visible";
 			if (button=="restore" && $scope.page=="del") return "img-visible";
 			if (button=="del" && $scope.page!="del") return "img-visible";
-			
 			return "img-hidden";
+		}
+		$scope.selectSection = function (section){
+			$scope.page=section;
+			$scope.section=$scope.sections[section];
+			angular.forEach($scope.sections,function(item){ item.css.data = "btn-info"; });
+			$scope.sections[section].css.data="btn-warning";
+			$scope.do_search();
+		}
+		$scope.changeUsername = function(username) {
+		  $scope.test = username;
+		}
+		$scope.img_move = function(img,event) {
+			$scope.img_div_style = 'left:'+(event.clientX+3)+'px;top:'+(event.clientY+3)+'px';
+		}
+		$scope.img_over = function(img) {
+			$scope.imgdiv='img-visible';
+			$scope.fullimg=img.emoji.url;
+		}
+		$scope.img_out = function() {
+			$scope.imgdiv='img-hidden';
+		}
+		$scope.do_search = function(){
+			//console.log($scope.search.id);
+			var filterd =  $filter('category')($scope.localEmojis, $scope);
+			//console.log(filterd);
+			$scope.found = $filter('searchById')(filterd, $scope.search.id);
+			
+			//console.log($scope.found);
+			
+			$scope.pagecount = Math.ceil($scope.found.length/10);
+			$scope.curpagen = 1;
+			$scope.pages = [];
+			$scope.pagesn = [];
+			
+			var onepage=[];
+			var n=0;
+			var pagen=0;
+			for(var i in $scope.found){
+				if(n==0){
+					$scope.pagesn.push(pagen);
+				}
+			
+				var item = $scope.found[i];
+				n++;
+				onepage.push(item);
+				if(n==10){
+					$scope.pages.push(onepage);
+					n=0;
+					onepage=[];
+					//if (pagen<10) $scope.pagesn.push(pagen);
+					pagen++;
+				}
+			}
+			if (n>0){
+				$scope.pages.push(onepage);
+				//pagen++;
+			}
+			
+			//$scope.pagesn = _.range(1, pagecount + 1);
+			$scope.curpage = $scope.pages[0];
+			$scope.setPage(0);
+			console.log($scope.pagesn);
+		}
+		$scope.setPage = function(page){
+			console.log(page);
+			if (page < 0 || page > $scope.pagecount-1) return;
+			$scope.curpagen = page;
+			if($scope.pagecount>10){
+				var start = $scope.curpagen-5; 
+				if (start < 0 ) start=0;
+				var end = start + 10;
+				
+				if( end > $scope.pagecount ){
+					end = $scope.pagecount;
+					start = $scope.pagecount - 10;
+				}
+			}else{
+				start=0;
+				end = $scope.pagecount;
+			}
+			$scope.pagesn=[];
+			for(var i = start;i<end;i++){
+				$scope.pagesn.push(i);
+			}
+			console.log($scope.pagesn);
+			$scope.curpage = $scope.pages[page];
+			
+		}
+		
+		//================================================
+		//    emoji actions
+		//------------------------------------------------
+		$scope.makeFavorive = function(emoji){
+			emoji.cat="fav";
+			emoji.css.data="img-no-transparent";
 		}
 		
 		$scope.restoreEmoji = function(emoji){
 			emoji.cat="all";
 			emoji.css.data="img-transparent";
+
+			var old_page = $scope.curpagen;
+			$scope.do_search();
+			if(old_page == $scope.pagecount) old_page = $scope.pagecount-1;
+			$scope.setPage(old_page);
 		}
 		
 		$scope.delEmoji = function(emoji){
@@ -95,23 +189,14 @@
 				emoji.cat="all";
 				emoji.css.data="img-transparent";
 			}
+			
+			var old_page = $scope.curpagen;
+			$scope.do_search();
+			if(old_page == $scope.pagecount) old_page = $scope.pagecount-1;
+			$scope.setPage(old_page);
 		}
-		
-		$scope.selectSection = function (section){
-			$scope.page=section;
-			$scope.section=$scope.sections[section];
-			angular.forEach($scope.sections,function(item){ item.css.data = "btn-info"; });
-			$scope.sections[section].css.data="btn-warning";
-		}
-
-		
-		$scope.changeUsername = function(username) {
-		  $scope.test = username;
-		};
-	
-	
+		//================================================
 		$scope.init();
-	
   }]);
   
 	myApp.filter('category', function() {
@@ -145,5 +230,17 @@
 		return undefined;
 	  }
 	});
+	
+	myApp.filter('searchById', function() {
+	  return function(input, id) {
+		var filtered=[];
+		for(var arr_id in input){
+			if (input[arr_id].id.indexOf(id) !== -1){
+				filtered.push(input[arr_id])
+			}
+		}
+		return filtered;
+	  }
+	});	
  
 })();
